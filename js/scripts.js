13279b3ba398a1ac73e6f4e7578357365cd90926
Trevor Teigen <trevor.teigen123@gmail.com>
2019-06-14 13:49:10 -0700

    working on getting the program to replace Dave with user name

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 $(function(){
+  var arr =[];
+  var dave = "I'm sorry Dave I can't do that"
 //Back End
 //create an array from 0-userInput;
   function count(number) {
-  for (var i = 0 ; i <= number ; i++) {
-    arr.push(i);
-  }
-  return arr;
-}
+    for (var i = 0 ; i <= number ; i++) {
+      arr.push(i);
+    };
+    return arr;
+};
 //check indices for 1,2,3 exceptions
   function rules(arr) {
     for (var i = 0; i < arr.length; i++) {
@@ -20,7 +22,17 @@ $(function(){
       } else if (check.indexOf('2') >= 0  &&  check.indexOf('3') < 0) {
         arr.splice(i,1,"Boop");
       } else if (check.indexOf('3') >= 0) {
-        arr.splice(i,1,"I'm sorry Dave I can't do that");
+        arr.splice(i,1,dave);
+      }
+    }
+  };
+//finds Daves and replaces them with the users' inputted name
+  function notDave(name) {
+    for (var i = 0; i < arr.length; i++) {
+      if (i%3 && ) {
+        alert(arr[i]);
+        var j = arr[i].split(' ');
+        j.splice(2,1,name)
       }
     }
   }
@@ -31,12 +43,14 @@ $(function(){
     event.preventDefault();
     var userInput = $("input#field1").val();
     var nameInput = $("inputefield2").val();
-    var arr = []; //clears the array so user make multiple attempts
+    arr = []; //clears the array so user make multiple attempts
     var numbers = count(userInput);
     rules(numbers);
+    notDave(nameInput);
     $("#result").text(arr);
   })
-}
+
+
 
 
 
